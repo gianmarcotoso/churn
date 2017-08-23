f586fd5d9f64459a68d2ce790e1c6996f51d2219
Gian Marco Toso <gianmarco.toso@gmail.com>
2017-08-23 15:48:12 +0200

    tweak super secret feature

--- src/churn.js
+++ src/churn.js
@@ -2,14 +2,14 @@ const mkdirp = require('mkdirp')
 const fs = require('fs')
 const path = require('path')
 
-const VARIABLES_FILE = '.variables'
+const TEMPLATE_DESCRIPTOR = '.template.js'
 
 const churn = (variables, fullPath, output) => {
 	mkdirp.sync(output)
 
 	const files = fs.readdirSync(fullPath)
 	files.forEach(element => {
-		if (element === VARIABLES_FILE) return
+		if (element === TEMPLATE_DESCRIPTOR) return
 
 		const filePath = path.join(fullPath, element)
 		if (fs.lstatSync(filePath).isDirectory()) {
